471c45078b1130e4358530aec630b172f81260e3
arushahg <75100016+arushahg@users.noreply.github.com>
2024-08-05 10:21:14 -0300

    Add files via upload

---
+++ UserModel.ts
@@ -0,0 +1,56 @@
+export class User{
+    private id: number;
+    private login: string;
+    private role: string;
+    private password: string;
+
+    constructor(id: number , login: string , role: string,  password: string) {
+        this.id = id;
+        this.login = login;
+        this.role = role;
+        this.password = password;
+    }
+
+    public getLogin(): string {
+        return this.login;
+    }
+
+    public setLogin(login: string): void {
+        if (login.trim().length === 0) {
+            throw new Error('O nome não pode ser vazio.');
+        }
+        this.login = login;
+    }
+
+    public getRole(): string {
+        return this.role;
+    }
+
+    public setRole(role: string): void {
+        if (role === 'admin'  role === 'medVet'  role === 'funcionario') {
+            this.role = role
+        }else{
+            this.role = 'unregisteredUser';
+        }
+    }
+
+    public getPassword(): string {
+        return this.password;
+    }
+
+    public setPassword(password: string): void {
+        if (password.trim().length === 0) {
+            throw new Error('O senha não pode ser vazio.');
+        }
+        this.password = password;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+}
